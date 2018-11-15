54a9d7f4d6b3b180cbe15299c911712079eb8b1c
bennettgberg <bennett.greenberg@rutgers.edu>
2018-11-15 16:22:48 -0500

    move null processing to compare

--- mergesort.c
+++ mergesort.c
@@ -4,6 +4,12 @@
 
 //return > 0 if A is larger, < 0 if B is larger, 0 if A = B.
 int compare(datarow A, datarow B, int index){
+	if(A.cells[index].is_empty == true){
+		return -1;
+	}
+	if(B.cells[index].is_empty == true) {
+		return 1;
+	}
 	if(A.cells[index].data_type == INT){
 		if(A.cells[index].num_datum > B.cells[index].num_datum) {
 			return 1;
@@ -68,25 +74,7 @@ datarow * sort(datarow * data, int ndata, int index) {
 
 
 datarow * mergesort(datarow * data, int index, int nrows ) {
-  //first do pre-processing and move all the null cells to the front.
-  //array to hold final sorted data
-	datarow * sorted_data = malloc(nrows*sizeof(datarow));
-  //array to hold the non-null data (to merge-sort)
-	datarow * to_sort = malloc(nrows*sizeof(datarow));
-	int n_null = 0;
-	int nto_sort = 0;
-	int i;
-	for(i = 0; i < nrows; ++i){
-		if(data[i].cells[index].is_empty == true){
-			sorted_data[n_null++] = data[i];
-		}
-		else {
-			to_sort[nto_sort++] = data[i];
-		}
-	}
-
-	to_sort = sort(to_sort, nto_sort, index);
-	memcpy(&sorted_data[n_null], to_sort, nto_sort*sizeof(datarow));
+//call sort function (compare() will take care of the nulls)
+	datarow * sorted_data = sort(data, nrows, index); 
 	return sorted_data;
-
 }
